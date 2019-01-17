b18deaeceba4a714640638ffc0e6cb8919725b6a
dev1 <dev@octopuscart.com>
2019-01-17 10:19:24 +0530

    changes

--- application/controllers/CustomApi.php
+++ application/controllers/CustomApi.php
@@ -582,7 +582,7 @@ class CustomApi extends REST_Controller {
                         "halfsleeve" => ["back_half_sleeve0001.png", "back_half_sleeve_cuff0001.png"],
                         "fullsleeve" => ["b_full_shirt_sleeve0001.png",],
                         "elements" => [ "b_shirtbody_round0001.png",],
-                        "overlay" => "",
+                        "overlay" => [],
                         "image" => "customization/back_plain.jpeg"
                     ), array(
                         "status" => "0",
@@ -590,7 +590,7 @@ class CustomApi extends REST_Controller {
                         "customization_category_id" => "5",
                         "halfsleeve" => ["back_half_sleeve0001.png", "back_half_sleeve_cuff0001.png"],
                         "fullsleeve" => ["b_full_shirt_sleeve0001.png", "b_full_shirt_sleeve0001.png",],
-                        "overlay" => ["back_two_side_plea_over_lay.png"],
+                        "overlay" => ["towside.png"],
                         "elements" => ["b_shirtbody_round0001.png", ],
                         "image" => "customization/back_two_side.jpeg"
                     ), array(
@@ -599,7 +599,7 @@ class CustomApi extends REST_Controller {
                         "customization_category_id" => "5",
                         "halfsleeve" => ["back_half_sleeve0001.png", "back_half_sleeve_cuff0001.png"],
                         "fullsleeve" => ["b_full_shirt_sleeve0001.png", "back_sleeve_cuff0001.png"],
-                        "overlay" => ["box_pleat_overlay.png"],
+                        "overlay" => ["boxpleat.png"],
                         "elements" => [ "b_shirtbody_round0001.png",   ],
                         "image" => "customization/back_box_pleat.jpeg"
                     ), array(
@@ -608,7 +608,7 @@ class CustomApi extends REST_Controller {
                         "customization_category_id" => "5",
                         "halfsleeve" => ["back_half_sleeve0001.png", "back_half_sleeve_cuff0001.png"],
                         "fullsleeve" => ["b_full_shirt_sleeve0001.png", "back_full_sleeve_cuff0001.png"],
-                        "overlay" => ["dart_overlay.png"],
+                        "overlay" => ["dartov.png"],
                         "elements" => ["b_shirtbody_round0001.png",  ],
                         "image" => "customization/dart.jpeg"
                     )],
