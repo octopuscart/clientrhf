ef68f100486271429a40d1d3737acd194a1a838a
dev1 <dev@octopuscart.com>
2019-02-15 09:14:03 +0530

    cahnges

--- application/views/Product/productList.php
+++ application/views/Product/productList.php
@@ -134,6 +134,13 @@ $image2 = "";
         }
         ?>
     }
+    
+    .gallery-items {
+    border: 1px solid #e0e0e0;
+    padding: 10px 10px;
+    height: 370px!important;
+    border-radius: 15px;
+}
 
 </style>
 
