a8cb60dbcf01b91c505ede177b026756d95af829
dev <dev@develop.com>
2019-09-19 15:31:54 +0530

    cahgnes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,4 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Shop';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-$route['appointment'] = "Shop/appointment2";
+$route['appointment'] = "Shop/appointment";
--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -363,6 +363,8 @@ class Shop extends CI_Controller {
     }
 
     public function appointment2() {
+        
+        redirect("appointment");
         $timeslot = [
             "07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM",
             "03:00 PM", "04:00 PM", "05:00 PM", "06:00 PM", "07:00 PM", "08:00 PM",
