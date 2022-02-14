d31934fb1ebe5f6670e9c20d823526fdbd60e473
octopuscart <dev@develop.com>
2022-02-14 14:35:21 +0530

    add files

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Rahman Fashions Hong Kong</title>
+  </head>
+  <body>
+  
+    <iframe src="assets/usa-trip-2022.pdf" width="100%" height="500px">
+    </iframe>
+  </body>
+</html>
